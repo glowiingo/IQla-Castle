72a1a025533c887bf7beb329305a7abb960dc093
Gloria Ngo <gloria.nhuto.ngo@gmail.com>
2020-10-02 00:30:35 -0700

    working backend

---
+++ src/game.js
@@ -0,0 +1,22 @@
+const config = {
+    type: Phaser.AUTO,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 600
+    },
+    backgroundColor: "0x000000",
+    physics: {
+        default: 'arcade',
+        arcade: {
+              enableBody: true,
+              debug: false,
+        }
+    },
+    scene: [preload_scene, mainmenu_scene, gameplay_scene]
+    // Specify scenes in the array above
+}
+console.log("game.js with specified config");
+
+const game = new Phaser.Game(config)
---
+++ src/scenes/mainmenu_scene.js
@@ -0,0 +1,11 @@
+class mainmenu_scene extends Phaser.Scene {
+    constructor() {
+        super("mainmenu_scene");
+    }
+
+    create() {
+        console.log("Main Menu");
+        this.add.text(20, 20, "Playing game", {font: "25px Arial", fill: "yellow"});
+        this.scene.start("gameplay_scene", {message: "test_data_from_main"});
+    }
+}
