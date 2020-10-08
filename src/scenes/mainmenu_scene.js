733ae5df3fe513dc2598bc92bbedf557a4174891
Gloria Ngo <gloria.nhuto.ngo@gmail.com>
2020-10-08 15:44:13 -0700

    added gameplay scene back

--- src/scenes/mainmenu_scene.js
+++ src/scenes/mainmenu_scene.js
@@ -5,14 +5,21 @@ class mainmenu_scene extends Phaser.Scene {
 
     create() {
         console.log("Main Menu");
-        this.mouse_click_text = this.add.text(20, 20, "Mouse Click Minigame", {font: "25px Arial", fill: "yellow"});
+        this.mouse_click_text = this.add.text(20, 20, "Gameplay Scene", {font: "25px Arial", fill: "yellow"});
+
+        this.mouse_click_text.setInteractive();
+        this.mouse_click_text.on("pointerdown", () => {
+            this.scene.start("gameplay_scene");
+        });
+
+        this.mouse_click_text = this.add.text(20, 60, "Mouse Click Minigame", {font: "25px Arial", fill: "yellow"});
 
         this.mouse_click_text.setInteractive();
         this.mouse_click_text.on("pointerdown", () => {
             this.scene.start("mouse_click_minigame");
         });
 
-        this.mouse_click_text = this.add.text(20, 60, "Book Click Minigame", {font: "25px Arial", fill: "yellow"});
+        this.mouse_click_text = this.add.text(20, 100, "Book Click Minigame", {font: "25px Arial", fill: "yellow"});
 
         this.mouse_click_text.setInteractive();
         this.mouse_click_text.on("pointerdown", () => {
