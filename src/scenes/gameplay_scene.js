df9c36c4b2aa652d5150bc8cf0a93aa55aad1970
BWhite1395 <barwhite7@gmail.com>
2020-10-15 16:55:50 -0700

    Added BGM
    Added a BGM.mp3 to new Audio folder in assets

--- src/scenes/gameplay_scene.js
+++ src/scenes/gameplay_scene.js
@@ -27,6 +27,7 @@ class gameplay_scene extends Phaser.Scene {
 
     this.load.tilemapTiledJSON('map', '../../assets/tilemaps/maps/protypeMap.json');
     this.load.image('tiles', '../../assets/tilemaps/tiles/drawtiles.png');
+    this.load.audio('BGM', '../../assets/audio/BGM.mp3');
   }
 
   create() {
@@ -58,6 +59,17 @@ class gameplay_scene extends Phaser.Scene {
 
     this.cameras.main.startFollow(this.player, true, 1, 1);
 
+    this.bgmusic = this.sound.add('BGM');
+    let musicConfig = {
+      mute: false,
+      volume: 1,
+      rate: 1,
+      detune: 0,
+      seek: 0,
+      loop: true,
+      delay: 0
+    }
+    this.bgmusic.play(musicConfig);
   }
 
   move_object_left_right(object, speed) {
@@ -74,6 +86,7 @@ class gameplay_scene extends Phaser.Scene {
   }
 
   player_movement(cursors) {
+    
     if (cursors.left.isDown) {
       // console.log("Down");
       //this.move_object_left_right(this.player, -10);
