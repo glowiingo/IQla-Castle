47e9e45cec7bd83c38fff23765baf3ac58ac01d0
Kotailri <willc8810@gmail.com>
2020-10-15 11:53:40 -0700

    Modified the map toggle to be invibile by default.

--- src/scenes/ui/mapOverlay_scene.js
+++ src/scenes/ui/mapOverlay_scene.js
@@ -1,36 +1,39 @@
 class mapOverlay_scene extends Phaser.Scene {
-  
-    constructor() {
-      super({
-        key: 'mapOverlay_scene',
-        active: true
-      });
-    }
-  
-    preload() {
-      this.load.image('map', '../../assets/mapOverlay.png');
-    }
-  
-    create() {
-  
-      this.mapOverlay = this.add.sprite(game.config.width/2, game.config.height/2, 'map');
-      this.mapOverlay.setScale(1);
 
-      this.enterPress = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-      this.enterPress.on('down',() =>this.mapHide());
+  constructor() {
+    super({
+      key: 'mapOverlay_scene',
+      active: true
+    });
+  }
 
-      this.spacePress = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
-      this.spacePress.on('down', () => this.mapShow());
+  preload() {
+    this.load.image('map', '../../assets/mapOverlay.png');
 
-    }
+  }
 
-    mapShow(){
-        this.mapOverlay.visible = true;
-        console.log('show');
-    }
+  create() {
+    this.scene.setVisible(false)
+    let showMap = false;
 
-    mapHide(){
-        this.mapOverlay.visible = false;
-        console.log('hide');
-    }
+    this.mapOverlay = this.add.sprite(game.config.width / 2, game.config.height / 2, 'map');
+    this.mapOverlay.setScale(1);
+
+    this.tabPress = this.input.keyboard.addKey('TAB');
+    this.tabPress.on('down', () => {
+      showMap = !showMap;
+      showMap ? this.mapShow() : this.mapHide();
+    });
+
+  }
+
+  mapShow() {
+    this.scene.setVisible(true)
+    console.log('show');
+  }
+
+  mapHide() {
+    this.scene.setVisible(false)
+    console.log('hide');
+  }
 }  
