6aeec1c6eab2227a4cb4401bd3a9042897d4b9a3
kdarakhshan <43010841+kdarakhshan@users.noreply.github.com>
2020-10-29 16:49:58 -0700

    Added code comments for future task bar

--- src/scenes/ui/playerUI_scene.js
+++ src/scenes/ui/playerUI_scene.js
@@ -1,4 +1,5 @@
 // Worked on by: William, Alexis
+// Worked on by: Bisht, Brian, Kian
 
 class playerUI_scene extends Phaser.Scene {
 
@@ -10,9 +11,15 @@ class playerUI_scene extends Phaser.Scene {
 
   preload() {
     this.load.image('kill', '../../assets/kill.png');
+    // This is to be added once the bar image is made: this.load.image('progress', '../../assets/progressbar.png')
   }
 
   create() {
+
+    // instantiate a progress bar in the top left corner of game screen, similar to the kill button
+    // note: add ProgressBar.increase(1) into each mini-game
+    // this.progressBar = this.add.sprite(0, 0, "progress");
+
 
     this.killButton = this.add.sprite(game.config.width - 100, game.config.height - 100, 'kill');
     this.killButton.setInteractive();
@@ -58,6 +65,7 @@ class playerUI_scene extends Phaser.Scene {
   resize(width, height) {
     //this.titleText.setPosition(document.body.offsetWidth / 2 - 300, 80);
     this.killButton.setPosition(document.body.offsetWidth - 200, document.body.offsetHeight - 200);
+
   }
 
 
