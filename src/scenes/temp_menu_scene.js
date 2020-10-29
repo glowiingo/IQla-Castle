91b83892b354f72ee895771129ee7d82f8e927b1
Gloria Ngo <gloria.nhuto.ngo@gmail.com>
2020-10-29 16:12:19 -0700

    updating temp menu scene formatting

--- src/scenes/temp_menu_scene.js
+++ src/scenes/temp_menu_scene.js
@@ -19,6 +19,7 @@ class temp_menu_scene extends Phaser.Scene {
     // this.mouse_click_text.on("pointerdown", () => {
     //     this.scene.start("YOUR_SCENE_NAME_HERE"); // Replace text.
     // });
+
     console.log("Temp Main Menu");
     // Worked on by: Evano
     this.serverConnection = new ServerConnection();
@@ -33,25 +34,24 @@ class temp_menu_scene extends Phaser.Scene {
       this.serverConnection.setRoom(r);
       this.scene.start("gameplay_scene", {serverConnection: this.serverConnection});
     });
+    
     this.mouse_click_text = this.add.text(20, 60, "Back End POC Menu", {
       font: "25px Arial",
       fill: "yellow"
     });
-
-        this.mouse_click_text.setInteractive();
-        this.mouse_click_text.on("pointerdown", () => {
-            // back_end_menu_scene
-            this.scene.start("mainmenu_scene");
-        });
-
-        this.mouse_click_text = this.add.text(20, 100, "Front End POC Menu", {
-            font: "25px Arial",
-            fill: "yellow",
-        });
-
-        this.mouse_click_text.setInteractive();
-        this.mouse_click_text.on("pointerdown", () => {
-            this.scene.start("title_screen_scene");
-        });
+    this.mouse_click_text.setInteractive();
+    this.mouse_click_text.on("pointerdown", () => {
+        // back_end_menu_scene
+        this.scene.start("mainmenu_scene");
+    });
+    
+    this.mouse_click_text = this.add.text(20, 100, "Front End POC Menu", {
+        font: "25px Arial",
+        fill: "yellow",
+    });
+    this.mouse_click_text.setInteractive();
+    this.mouse_click_text.on("pointerdown", () => {
+        this.scene.start("title_screen_scene");
+    });
     }
 }
