48ebd531d6adbeec39a0cdf78cbd3ccda94cd11d
Charles <charleshuang97@gmail.com>
2020-11-03 20:42:23 -0800

    fixed bug where mouse doesn't go to edge of canvas

--- src/scenes/mouse_click_minigame.js
+++ src/scenes/mouse_click_minigame.js
@@ -1,5 +1,6 @@
 // Worked on by: Charles Huang
 const NUM_OF_MICE = 3;
+const MOUSE_SCALE = 0.6;
 const MIN_DUR = 200; // minimun amount of time in ms for a mouse to travel from A to B
 const MAX_DUR = 600; // maximum amount of time in ms for a mouse to travel from A to B
 
@@ -28,15 +29,16 @@ class mouse_click_minigame extends Phaser.Scene {
     );
     background.displayWidth = data.width;
     background.displayHeight = data.height;
-    
-    let mouseWidth = game.textures.get('pp').source[0].width;
-    let mouseHeight = game.textures.get('pp').source[0].height;
+
+    // setScale does not affect image width/height so separate variables are created
+    let mouseWidth = this.scene.scene.textures.get('pp').getSourceImage().width * MOUSE_SCALE;
+    let mouseHeight = this.scene.scene.textures.get('pp').getSourceImage().height * MOUSE_SCALE;
     let mice = [];
 
     for(let i = 0; i < NUM_OF_MICE; i++){
       let randX = this.getRandomWidthFloat(data.width, mouseWidth);
       let randY = this.getRandomHeightFloat(data.height, mouseHeight);
-      mice.push(this.add.image(randX, randY, 'pp').setScale(0.6));
+      mice.push(this.add.image(randX, randY, 'pp').setScale(MOUSE_SCALE));
     }
     let _this = this; // Idk why but this is suddenly no longer needed and nothing regarding this was change wtf js this is why no one likes you fucking piece of shit like srs how has no one fucking made a replacement for js fucking christ man so many languages i mean we got C, C++, C#, and Object C and that's just one fucking letter and you're gonna tell me out of all these plethora platypuses of languages the shittiest one just happened to be the only option we have for web? This is like the reverse of Mr.Strange's time stone probability thingy thang but instead of winning we end up with this fucking situation with js fucks sake ty for listening to my ted talk. // wait nvm I still need _talk but only one of my "this" needs this and literally every other this doesn't so my point still stands fuck you js. oh hey found a way to not use it but once more my point still stands fuck u mike fuck you.
     
@@ -45,7 +47,7 @@ class mouse_click_minigame extends Phaser.Scene {
       mouse.setInteractive();
       mouse.on('pointerdown', function() {
         mice.splice(mice.indexOf(mouse), 1);
-        tween.stop();
+        //tween.stop();
         mouse.setVisible(false);
         
         if (mice.length === 0){
@@ -53,7 +55,7 @@ class mouse_click_minigame extends Phaser.Scene {
         }
       });
       
-      // move objects to random coordinates on the board at random speeds.
+      //move objects to random coordinates on the board at random speeds.
       mouse.nextX = this.getRandomWidthFloat(data.width, mouseWidth);
       mouse.nextY = this.getRandomHeightFloat(data.height, mouseHeight);
       mouse.rotation = this.getRadians(mouse.x, mouse.y, mouse.nextX, mouse.nextY);
@@ -90,12 +92,12 @@ class mouse_click_minigame extends Phaser.Scene {
   // a random float between 2.5 and 3.5 (exclusive).
   getRandomWidthFloat(board_size, object_size){
     let min = (this.game.config.width - board_size) / 2 + object_size / 2;
-    return Math.random() * (board_size - object_size / 2 - min) + min;
+    return Math.random() * (board_size - object_size) + min;
   }
 
   getRandomHeightFloat(board_size, object_size){
     let min = (this.game.config.height - board_size) / 2 + object_size / 2;
-    return Math.random() * (board_size - object_size / 2 - min) + min;
+    return Math.random() * (board_size - object_size) + min;
   }
 
   // atan2() takes in y param first and returns randians relative to
