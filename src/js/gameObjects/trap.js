3f8df24c22ef7f84f885e8d44f8438405a40abec
kiwon <kiwon132445@gmail.com>
2020-11-05 12:51:37 -0800

    minor trap edits before merging

--- src/js/gameObjects/trap.js
+++ src/js/gameObjects/trap.js
@@ -1,12 +1,12 @@
 //Worked on Kiwon
 
 class Trap extends Phaser.GameObjects.Sprite {
-    constructor (config) {
+    constructor (config, playerGroup) {
         super(config.scene, config.x, config.y, 'trap');
         this.scene = config.scene;
         this.x = config.x;
         this.y = config.y;
-        this.playerGroup;
+        this.playerGroup = playerGroup;
         this.setTrap();
         this.trapSet = false;
 
@@ -15,6 +15,7 @@ class Trap extends Phaser.GameObjects.Sprite {
     in_trap_radius() {
         let touching = this.trapZone.body.touching;
         let wasTouching = this.trapZone.body.wasTouching;
+        let inZone = this.trapZone.body.embedded;
         
         if (touching.none && !wasTouching.none) {
             this.scene.player1.clearTint();
@@ -24,6 +25,11 @@ class Trap extends Phaser.GameObjects.Sprite {
             this.scene.player1.setTint(0x00ffff);
             this.trapZone.emit('enterzone');
         }
+        else if(inZone) {
+            this.scene.player1.setTint(0x00ffff);
+            this.trapZone.emit('inzone');
+            console.log("inzone");
+        }
     
         this.trapZone.body.debugBodyColor = this.trapZone.body.touching.none ? 0x00ffff : 0xffff00;
     }
@@ -31,30 +37,31 @@ class Trap extends Phaser.GameObjects.Sprite {
 
     setTrap() {
         if (!this.trapSet) {
-            this.scene.add.existing(this).setScale(0.5);
+            this.scene.add.existing(this).setScale(1);
 
-            this.trapZone = this.scene.add.zone(this.x, this.y).setSize(this.displayWidth, this.displayHeight);
-            this.trapZone.setCircleDropZone(100);
-            this.scene.physics.world.enable(this.trapZone, 0); // (0) DYNAMIC (1) STATIC
-            this.trapZone.body.setAllowGravity(false);
-            this.trapZone.body.moves = false;
+            this.trapZone = this.scene.add.zone(this.x, this.y).setSize(this.displayWidth, this.displayWidth);
+            this.trapZone.setCircleDropZone(this.displayWidth/2);
+            this.scene.physics.world.enable(this.trapZone, 1); // (0) DYNAMIC (1) STATIC
 
-            this.scene.physics.add.overlap(this.trapZone, this.scene.player1);
-            
-            this.trapZone.on('enterzone', ()=> {
-                this.activateTrap();
-            });
+            this.trapSet = true;
+
+            this.scene.physics.add.overlap(this.trapZone, this.playerGroup, this.activateTrap, null, this);
         }
-        this.trapSet = true;
+        
     }
     activateTrap() {
-        this.blastZone = this.scene.add.zone(this.x, this.y).setSize(this.displayWidth * 2, this.displayHeight * 2);
-        this.blastZone.setCircleDropZone(100);
-        this.scene.physics.world.enable(this.blastZone, 0); // (0) DYNAMIC (1) STATIC
-        this.blastZone.body.setAllowGravity(false);
-        this.blastZone.body.moves = false;
-        this.scene.physics.add.overlap(this.blastZone, this.scene.player1, this.scene.kill/*, this.scene.playerAlive*/);
+        if (this.trapTriggered) {
+            console.log("triggered");
+            this.blastZone = this.scene.add.zone(this.x, this.y).setSize(this.displayHeight, this.displayHeight);
+            this.blastZone.setCircleDropZone(this.displayHeight/2);
+            this.scene.physics.world.enable(this.blastZone, 0); // (0) DYNAMIC (1) STATIC
+            this.blastZone.body.setAllowGravity(false);
+            this.blastZone.body.moves = false;
+            this.trapTriggered = true;
+            this.scene.physics.add.overlap(this.blastZone, this.playerGroup, this.scene.kill/*, this.scene.playerAlive*/);
+        }
     }
+
     checkKillZone() {
 
     }
