dc25ed2464eccd3b520205f2cff191f4dcf95008
flemming chow <flemming@flemmings-MacBook-Pro.local>
2020-11-05 17:32:20 -0800

    implemented some timer functionality

--- src/scenes/gameplay_scene.js
+++ src/scenes/gameplay_scene.js
@@ -5,18 +5,16 @@ This should be a POC for front end, logic needs to be separated for the map.
 */
 
 class gameplay_scene extends Phaser.Scene {
-
   // Worked on by: Gloria Ngo
   constructor() {
     super({
-      key: 'gameplay_scene'
+      key: "gameplay_scene",
     });
   }
 
-  
   // Worked on by: Gloria Ngo
   init(data) {
-    // initialize and prepare data 
+    // initialize and prepare data
     // constants, configurations, etc.
     this.message = data.message; // scene var called message passed in to scene
 
@@ -29,12 +27,19 @@ class gameplay_scene extends Phaser.Scene {
   preload() {
     // load audio and images into memory
     // this.load.image('haachama', '../../assets/player/Player.png');
-    this.load.spritesheet('haachama', '../../assets/player/PlayerWalkCycle.png', { frameWidth: 128, frameHeight: 128, endFrame: 7 });
+    this.load.spritesheet(
+      "haachama",
+      "../../assets/player/PlayerWalkCycle.png",
+      { frameWidth: 128, frameHeight: 128, endFrame: 7 }
+    );
 
-    this.load.tilemapTiledJSON('map', '../../assets/tilemaps/maps/protypeMap.json');
-    this.load.image('tiles', '../../assets/tilemaps/tiles/updated-tiles.png');
-    this.load.image('deadbody', 'assets/deadCharacter.png');
-    this.load.audio('BGM', '../../assets/audio/BGM.mp3');
+    this.load.tilemapTiledJSON(
+      "map",
+      "../../assets/tilemaps/maps/protypeMap.json"
+    );
+    this.load.image("tiles", "../../assets/tilemaps/tiles/updated-tiles.png");
+    this.load.image("deadbody", "assets/deadCharacter.png");
+    this.load.audio("BGM", "../../assets/audio/BGM.mp3");
   }
 
   create() {
@@ -42,6 +47,7 @@ class gameplay_scene extends Phaser.Scene {
     console.log("gameplay_scene");
 
     this.scene.launch("playerUI_scene");
+    this.scene.launch("timer_scene");
     this.scene.launch("mapOverlay_scene");
     this.scene.launch("showPositionPlayer_scene");
     this.scene.launch("voting_scene");
@@ -50,14 +56,14 @@ class gameplay_scene extends Phaser.Scene {
     this.isWalking = false;
 
     let config = {
-      key: 'WalkCycle',
-      frames: this.anims.generateFrameNumbers('haachama', { start: 0, end: 7 }),
+      key: "WalkCycle",
+      frames: this.anims.generateFrameNumbers("haachama", { start: 0, end: 7 }),
       frameRate: 8,
-      repeat: -1
+      repeat: -1,
     };
     this.anims.create(config);
 
-    this.bgmusic = this.sound.add('BGM');
+    this.bgmusic = this.sound.add("BGM");
     let musicConfig = {
       mute: false,
       volume: 0.5,
@@ -65,18 +71,18 @@ class gameplay_scene extends Phaser.Scene {
       detune: 0,
       seek: 0,
       loop: true,
-      delay: 0
-    }
+      delay: 0,
+    };
     this.bgmusic.play(musicConfig);
 
     // Worked on by: Flemming, William
-    let map = this.make.tilemap({ key: 'map' });
-    let tileset = map.addTilesetImage('updated_tiles', 'tiles')
-    map.createStaticLayer('Background', tileset);
-    map.createStaticLayer('Ground', tileset);
-    map.createStaticLayer('Interactables', tileset);
+    let map = this.make.tilemap({ key: "map" });
+    let tileset = map.addTilesetImage("updated_tiles", "tiles");
+    map.createStaticLayer("Background", tileset);
+    map.createStaticLayer("Ground", tileset);
+    map.createStaticLayer("Interactables", tileset);
 
-    this.wallsLayer = map.createStaticLayer('Walls', tileset);
+    this.wallsLayer = map.createStaticLayer("Walls", tileset);
     this.wallsLayer.setCollisionByProperty({ collides: true });
 
     // Worked on by: Evano
@@ -84,16 +90,17 @@ class gameplay_scene extends Phaser.Scene {
     this.sceneData.serverConnection.addGameplayHandlers(this.sceneData);
     this.sceneData.serverConnection.joinRoom();
   }
-// Worked on by: Gloria Ngo
+  // Worked on by: Gloria Ngo
   update() {
-    // loop that runs constantly 
+    // loop that runs constantly
     // -- game logic mainly in this area
-    if(this.player){
+    if (this.player) {
       this.player.player_movement();
       this.sceneData.serverConnection.movement(this.player);
-      this.scene.get('showPositionPlayer_scene').move(this.player.x, this.player.y);
+      this.scene
+        .get("showPositionPlayer_scene")
+        .move(this.player.x, this.player.y);
     }
-
   }
 
   vote(votedFor) {
@@ -101,41 +108,50 @@ class gameplay_scene extends Phaser.Scene {
   }
 
   // Worked on by: Evano
-    addPlayer(playerInfo) {
-        console.log("PLAYERINFO:", playerInfo);
-        this.player = new Player({
-          scene:this, 
-          x: playerInfo.x, 
-          y: playerInfo.y, 
-          sprite:'haachama'
-      }, playerInfo.playerId, "john", 300);
+  addPlayer(playerInfo) {
+    console.log("PLAYERINFO:", playerInfo);
+    this.player = new Player(
+      {
+        scene: this,
+        x: playerInfo.x,
+        y: playerInfo.y,
+        sprite: "haachama",
+      },
+      playerInfo.playerId,
+      "john",
+      300
+    );
 
-        this.add.existing(this.player).setScale(1);
-        this.physics.add.existing(this.player);
+    this.add.existing(this.player).setScale(1);
+    this.physics.add.existing(this.player);
 
-        this.player.body.offset.y = 64;
-        this.player.body.offset.x = 32;
-        this.player.body.height = 64;
-        this.player.body.width = 64;
-        
-        this.physics.add.collider(this.player, this.wallsLayer);
-        this.cameras.main.startFollow(this.player, true, 1, 1);
-        return this.player;
-    }
+    this.player.body.offset.y = 64;
+    this.player.body.offset.x = 32;
+    this.player.body.height = 64;
+    this.player.body.width = 64;
 
-    addOtherPlayer(playerInfo) {
-        const otherPlayer = new Player({
-          scene:this, 
-          x: playerInfo.x, 
-          y: playerInfo.y, 
-          sprite:'haachama'
-      }, playerInfo.playerId, "john", 300);
-      
-        //otherPlayer.setTint(0xff0000); Sets tint of other players to red for testing purposes
-       
-        this.add.existing(otherPlayer).setScale(1);
-        this.otherPlayers.add(otherPlayer);
-        return otherPlayer;
-    }
+    this.physics.add.collider(this.player, this.wallsLayer);
+    this.cameras.main.startFollow(this.player, true, 1, 1);
+    return this.player;
+  }
+
+  addOtherPlayer(playerInfo) {
+    const otherPlayer = new Player(
+      {
+        scene: this,
+        x: playerInfo.x,
+        y: playerInfo.y,
+        sprite: "haachama",
+      },
+      playerInfo.playerId,
+      "john",
+      300
+    );
+
+    //otherPlayer.setTint(0xff0000); Sets tint of other players to red for testing purposes
+
+    this.add.existing(otherPlayer).setScale(1);
+    this.otherPlayers.add(otherPlayer);
+    return otherPlayer;
+  }
 }
-
---
+++ src/scenes/ui/timer_scene.js
@@ -0,0 +1,26 @@
+// worked on by flemming
+class timer_scene extends Phaser.Scene {
+  constructor() {
+    super({
+      key: "timer_scene",
+    });
+    console.log("timer_scene");
+  }
+
+  preload() {
+    this.load.image("no", "../../assets/exitButt.png");
+  }
+  create() {
+    console.log("timer button created");
+    let killBtn = new timerButton(
+      {
+        scene: this,
+        x: game.config.width - 100,
+        y: game.config.height - 50,
+        sprite: "no",
+      },
+      20
+    );
+    killBtn.setObj(this);
+  }
+}
