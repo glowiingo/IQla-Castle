f7fefa31ae938fc7c18cec426b3c531203941450
Kotailri <willc8810@gmail.com>
2020-11-13 13:30:15 -0800

    fix style

--- src/scenes/ui/chat_scene.js
+++ src/scenes/ui/chat_scene.js
@@ -3,7 +3,7 @@
 class chat_scene extends Phaser.Scene {
   constructor() {
     super({
-      key: "chat_scene",
+      key: 'chat_scene',
     });
   }
 
@@ -17,21 +17,21 @@ class chat_scene extends Phaser.Scene {
     const screenX = this.cameras.main.width;
     const screenY = this.cameras.main.height;
 
-    this.textbox = document.getElementById("textbox");
-    //this.textbox.style.display = "block";
+    this.textbox = document.getElementById('textbox');
+    //this.textbox.style.display = 'block';
 
-    this.chatbox = document.getElementById("chatbox");
-    //this.chatbox.style.display = "block";
+    this.chatbox = document.getElementById('chatbox');
+    //this.chatbox.style.display = 'block';
 
     // temp scene activation
-    this.keyPress = this.input.keyboard.addKey("CTRL");
-    this.keyPress.on("down", () => {
+    this.keyPress = this.input.keyboard.addKey('CTRL');
+    this.keyPress.on('down', () => {
       this.toggleVisible();
     });
 
     // bypass player movement inputs
     window.addEventListener('keydown', function(e) {
-      if(e.keyCode == 32) {
+      if(e.keyCode === 32) {
         e.preventDefault();
       }
     });
@@ -39,15 +39,15 @@ class chat_scene extends Phaser.Scene {
     this.input.keyboard.on('keydown', (event) => {
       if (this.showChat === true) {
         if (event.key.length === 1) {
-          document.getElementById("textbox").value += event.key;
-        } else if (event.key === "Backspace") {
-          let str = document.getElementById("textbox").value;
-          document.getElementById("textbox").value = str.substring(0, str.length - 1);
-        } else if (event.key === "Enter") {
-          if (document.getElementById("textbox").value !== "") {
-            let name = this.scene.get("gameplay_scene").player.playerName;
-          this.scene.get("gameplay_scene").sceneData.serverConnection.sendMessage(name, document.getElementById("textbox").value);
-          document.getElementById("textbox").value = "";
+          document.getElementById('textbox').value += event.key;
+        } else if (event.key === 'Backspace') {
+          let str = document.getElementById('textbox').value;
+          document.getElementById('textbox').value = str.substring(0, str.length - 1);
+        } else if (event.key === 'Enter') {
+          if (document.getElementById('textbox').value !== '') {
+            let name = this.scene.get('gameplay_scene').player.playerName;
+          this.scene.get('gameplay_scene').sceneData.serverConnection.sendMessage(name, document.getElementById('textbox').value);
+          document.getElementById('textbox').value = '';
           }
         }
       }
@@ -56,24 +56,24 @@ class chat_scene extends Phaser.Scene {
   }
 
   receiveMsg(name, text) {
-    this.chatbox.value += name + ": " + text + "\r\n";
+    this.chatbox.value += name + ': ' + text + '\r\n';
   }
 
 
 
   toggleVisible() {
-    this.scene.get("gameplay_scene").player.setVelocityX(0);
-    this.scene.get("gameplay_scene").player.setVelocityY(0);
-    this.scene.get("gameplay_scene").player.player_walk_anim_stop();
+    this.scene.get('gameplay_scene').player.setVelocityX(0);
+    this.scene.get('gameplay_scene').player.setVelocityY(0);
+    this.scene.get('gameplay_scene').player.player_walk_anim_stop();
     this.showChat = !this.showChat;
     if (this.showChat) {
       this.scene.setVisible(true);
-      this.textbox.style.display = "block";
-      this.chatbox.style.display = "block";
+      this.textbox.style.display = 'block';
+      this.chatbox.style.display = 'block';
     } else {
       this.scene.setVisible(false);
-      this.textbox.style.display = "none";
-      this.chatbox.style.display = "none";
+      this.textbox.style.display = 'none';
+      this.chatbox.style.display = 'none';
     }
   }
 }
